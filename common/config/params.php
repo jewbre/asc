9d247751686bd5d63bab3a3db2c3b67f16fa2243
jewbre <jewbre18@gmail.com>
2017-05-14 13:27:28 +0200

    push thisπ

--- common/config/params.php
+++ common/config/params.php
@@ -6,7 +6,7 @@ return [
 
     'facebookAppID' => '206672269849809',
     'facebookSecret' => 'a64f74ddef838956f256a2d5c4e35ec2',
-    'googleClientID' => '879947320620-prvcuhtrhb2rd4fu3a2t8qf1ckga6d28.apps.googleusercontent.com',
+    'googleClientID' => '879947320620-6iksif9k13gonk1qd97to82g89qu5e7v.apps.googleusercontent.com',
 
     'postmarkToken' => '8c32ea60-2948-4a6a-bd28-e39f218b4ad8',
     'welcomeMailID' => '1798622',
