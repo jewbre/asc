f87b87ffc2b4a60071b8359c4682559fc8ba5bcf
jewbre <jewbre18@gmail.com>
2017-06-03 15:08:23 +0200

    pitch sentence fixπ

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -12,7 +12,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-login">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p class="grey-text subtitle">Relax and enjoy you life with other people.</p>
+    <p class="grey-text subtitle">Relax and enjoy your life with other people.</p>
 
     <button id="facebook-login" class="fb-btn">Facebook</button>
     <div class="g-signin2 google-btn" data-onsuccess="onGoogleSignIn">Google</div>
